20541a704a4b5371967a87c14a22fe691824b3e9
marie <kmarieren@gmail.com>
2025-04-02 11:47:34 -0400

    Class Country and CountryTest created and completed...  made some changes, please review

--- src/TeamMembers/Marie/CountryClients.java
+++ src/TeamMembers/Marie/CountryClients.java
@@ -8,8 +8,11 @@ public class CountryClients {
     Country country1 = new Country("Tajikistan", 11.00, "Asia");
         System.out.println(country1);
 
-        Country country2 = new Country("USA", 340, "South America");
+        Country country2 = new Country("USA", 340, "North America");
         System.out.println(country2);
+
+        System.out.println(Country.worldPopulation + " billion");
+        System.out.println(Country.co2Level + " ppm");
 
     }
 
