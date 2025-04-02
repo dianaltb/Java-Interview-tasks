86e61b2075a655764cc646d72c4f9e908c0f42fe
rasanbg <153374653+rasanbg@users.noreply.github.com>
2025-04-02 17:08:55 +0200

    finished task 1

---
+++ src/TeamMembers/Rasa/Country.java
@@ -0,0 +1,39 @@
+package TeamMembers.Rasa;
+
+public class Country {
+
+    public static double worldPopulation;
+    public static double worldCo2Level;
+
+    public String name;
+    public double population;
+    public String continent;
+
+    static {
+        worldPopulation = 7.88;
+        worldCo2Level = 417.83;
+    }
+
+    public static double getWorldPopulation() {
+        return worldPopulation;
+    }
+
+    public static double getWorldCo2Level() {
+        return worldCo2Level;
+    }
+
+    public Country(String name, double population, String continent) {
+        this.name = name;
+        this.population = population;
+        this.continent = continent;
+    }
+
+    @Override
+    public String toString() {
+        return "Country{" +
+                "name='" + name + '\'' +
+                ", population=" + population +
+                ", continent='" + continent + '\'' +
+                '}';
+    }
+}
