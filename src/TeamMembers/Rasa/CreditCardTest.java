743455a8313942ad34939c1c6684be84e23e99be
rasanbg <153374653+rasanbg@users.noreply.github.com>
2025-04-08 11:29:23 +0200

    finished task 3

---
+++ src/TeamMembers/Rasa/CreditCardTest.java
@@ -0,0 +1,12 @@
+package TeamMembers.Rasa;
+
+public class CreditCardTest {
+    public static void main(String[] args) {
+
+        CreditCard card = new CreditCard("american express", "123456789012345", "Roger Jones", 1201);
+        System.out.println(card);
+
+
+
+    }
+}
