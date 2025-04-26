ae77931239d1db26c3e752b94d6b027dc378f9ca
sashk <sashkovnyc@gmail.com>
2025-04-26 13:33:00 -0400

    BinaryRepresentation task is done.

--- src/TeamMembers/Egor/BinaryRepresentation.java
+++ src/TeamMembers/Egor/BinaryRepresentation.java
@@ -1,7 +1,25 @@
 package TeamMembers.Egor;
 
+import java.util.Random;
+
 public class BinaryRepresentation {
     public static void main(String[] args) {
-        System.out.println("sup");
+        Random random = new Random();
+        int num = random.nextInt(50) + 1;
+
+        howManyOnes(num);
+        //47 has the binary representation of 101111. So the number of "1" is: 5.
+    }
+
+    public static void howManyOnes(int num){
+        String binary = Integer.toBinaryString((num));
+        int count = 0;
+        for(char ch : binary.toCharArray()){
+            if(ch == '1'){
+                count++;
+            }
+        }
+        System.out.println(num + " has the binary representation of " +
+                binary + ". So the number of \"1\" is: " + count + ".");
     }
 }
